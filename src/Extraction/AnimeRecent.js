094751a6176c4e81cdf67e92c7754e9e4b7b1a3b
Danyspb4ever <danyspb4ever@gmail.com>
2023-08-12 17:44:05 +0000

    refactor: modifier la strucure de mon code

--- src/Extraction/AnimeRecent.js
+++ src/Extraction/AnimeRecent.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { RecentAnimeLink, Domaine } = require('../Liens/AnimeLink');
 
 
-const url = "https://www.ianimes.org/index.php";
+const url = RecentAnimeLink;
 const dataAnime = [];
 
 async function RecentAnime(){
@@ -12,11 +13,11 @@ async function RecentAnime(){
     const $ = cheerio.load(donnes.data)
 
     $('center td[align="center"]').each((i,inf)=>{
-        const domaine = 'https://www.ianimes.org/';
+        
         const titre = $(inf).find('td[colspan="3"] span').text();
         const image =  $(inf).find('div').css('background').match(/http.*(jpg|png|jpeg|webp)/gm).toString();
         const lienText = $(inf).find('a').attr('href');
-        const lien = `${domaine}${lienText}`;
+        const lien = `${Domaine}${lienText}`;
         const episode = $(inf).find('font[color="#FF4500"]').text();
         const type = $(inf).find('font[color="#008080"]').eq(1).text();
         const result = {titre , image, lien, episode, type}
---
+++ src/Liens/AnimeLink.js
@@ -0,0 +1,9 @@
+
+const RecentAnimeLink = 'https://www.ianimes.org/index.php';
+const Domaine = 'https://www.ianimes.org/';
+
+
+module.exports = {
+    RecentAnimeLink,
+    Domaine
+}

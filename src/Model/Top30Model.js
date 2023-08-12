2a44bf0d518a407b1264972d83213ca0645260eb
Danyspb4ever <danyspb4ever@gmail.com>
2023-08-12 22:06:57 +0000

    test: test ok pour le top30anime

--- src/Model/Top30Model.js
+++ src/Model/Top30Model.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-
-
 const Top30Schema = new mongoose.Schema({
     titre: String,
     image: String,
--- src/Routes/AnimeRoutes.js
+++ src/Routes/AnimeRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const RecentAnime = require('../Extraction/AnimeRecent');
 const { RecentModel } = require('../Model/RecentModel');
+const LesTop30Animes = require('../Extraction/Top30Anime');
+const { Top30Model } = require('../Model/Top30Model');
 const router = express.Router();
 
 
@@ -19,7 +21,25 @@ router.get(`/recent`,async(req, res)=>{
     }
 })
 
+
+router.get(`/top30`,async(req, res)=>{
+    try{
+        const data = await LesTop30Animes();
+        res.status(200).json({succes: true, data})
+    }catch{
+        if(res.status(404)){
+            return res.json({succes: false, message: 'erreur aucun fichier trouver '})
+        }
+        if(res.status(500)){
+            return res.status(500).json({succes: false, message: ' Erreur Arrete de faire de la merde !!!'})
+        }
+                
+    }
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////// si vous voulez recuperer les donnes de votre base de donnes  utilisez le chemin ci dessous    //////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 router.get(`/dbRecent`, async(req, res)=>{
     let recentAniDB = await RecentModel.find();
@@ -31,6 +51,20 @@ router.get(`/dbRecent`, async(req, res)=>{
         }
     }else{
         return res.status(200).json(recentAniDB)
+    }
+   
+})
+
+router.get(`/dbTop30`, async(req, res)=>{
+    let top30 = await Top30Model.find();
+    if(!top30){
+        if(res.status(500)){
+            return  res.status(500).json({succes: false, message: 'Erreur Arrete de faire de la merde !!!'})
+        }if(res.status(404)){
+            return res.status(404).json({succes: false, message: 'Erreur Fichier non trouvee'})
+        }
+    }else{
+        return res.status(200).json(top30)
     }
    
 })

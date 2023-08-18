b067c478ebf5478a5628b7120dc6c7300b48ae79
Danyspb4ever <danyspb4ever@gmail.com>
2023-08-18 02:02:22 +0000

    test: tester les infos recuperer

--- src/Extraction/Animes/InfosAnime.js
+++ src/Extraction/Animes/InfosAnime.js
@@ -12,20 +12,60 @@ async function DetailsAnime() {
         const donnes = await axios.get(url);
         const $ = cheerio.load(donnes.data)
 
-        const genres = [];
-        const titre = $('center h1 ').text();
-        const CoverImage = $('center div ').css('background').match(/http.*(jpg|png|jpeg|webp)/gm).toString();
-        const image =  $('td img').attr('src');
-        const titreOriginal = $('td font:contains("Titre original")').next().text();
-        const type = $('td font:contains("Format")').next().text();
+        const episodes = [];
+        const titreOriginal = '';
+        const type = '';
+        const genres = '';
+        const dateSortie = '';
+        const dureeEpisode = '';
+        const studio = '';
         
 
 
+    
+        const titre = $('center h1 ').text();
+        const coverImage = $('center div ').css('background').match(/http.*(jpg|png|jpeg|webp)/gm).toString();
+        const image =  $('td img').attr('src');
+        titreOriginal = $('td font:contains("Titre original")').next().text();
+        type = $('td font:contains("Format")').next().text();
+        genres = $('td font:contains("Genre")').next().text();
+        dateSortie = $('td font:contains("Date de Diffusion")').next().text();
+        dureeEpisode = $('td font:contains("Durée par épisode")').next().text();
+        studio = $('td font:contains("Studio")').next().text();
+        const description = $('legend').eq(1).next().text();
+        $('.cat_post_item-1.clearfix a').each((i, ep)=>{
+            episodes.push(
+               numero = $(ep).text(),
+                lien = $(ep).attr('href')
+            )
+             
+        })
+
+        const result = {
+            
+            titre,
+            coverImage,
+            image,
+            titreOriginal,
+            type,
+            genres,
+            dateSortie,
+            dureeEpisode,
+            studio,
+            episodes     
+            
+        }
+        dataAnime.push(result);
+        console.log(dataAnime)
         
 
 
     }catch{
-
+        console.log('helllo')
     }
 
+    DetailsAnime();
+
 }
+
+module.exports = DetailsAnime;

37d012635b5dd52fba6d8789ea1524a576eea9b8
Danyspb4ever <danyspb4ever@gmail.com>
2023-08-20 23:24:44 +0000

    feat: scrapping pagination by Alphebet

--- src/Liens/AnimeLink.js
+++ src/Liens/AnimeLink.js
@@ -9,43 +9,38 @@ const AnimeInfo = `https://www.ianimes.org/liste.php?manga=BbkCdhihfKnKBPQM`;
 
                      //////////////////// liste  des animes par ordre alphabetic ////////////////
 
-
-
 const page = [{
-    debA: Xdf789POGg45rrty,
-    A: Qxc108FGkpodhjccjv,
-    B: BoR851UkN56456uui,
-    C: ADs480FvP01121j,
-    D: Wup331BgFuytt788o,
-    E: Vja882Gptlklgjjk,
-    F: CbO016Dxmipoifdg5456,
-    G: Jpr104Gol654rtetfgh,
-    H: Bss541Lky987ljhlgh2313s,
-    I: FtH029Lgtjkhnbspoipfg,
-    J: Mer710Gnr132654987,
-    K: Qop487Rdtkmlkfgh5646aze,
-    L: Bod452VViiuher8787fd,
-    M: Wqs418Rtbsdfg7987ds,
-    N: SpS891Ntpztzret87ty,
-    O: Xod038Fbzfghfdh87df,
-    P: ApG097Bgtzrte787dfg,
-    Q: Pvi726Butcvbqs45re,
-    R: Foo00451xcv45fdtyr,
-    S: Kpy369Mbvzzret7987,
-    T: PdR780Bop78ret89z7g,
-    U: Mvp442Srifdsg7987sdg,
-    V: Nrs598Ftp87rfdxgxd8,
-    W: Cof591Fopoiup87uipou,
-    X: Jsa419Ygpazer878fdsd,
-    Y: Rij126Prtezrt5454sdfg,
-    Z: QQo447FFtxvxw7987
+    debA: 'Xdf789POGg45rrty',
+    A: 'Qxc108FGkpodhjccjv',
+    B: 'BoR851UkN56456uui',
+    C: 'ADs480FvP01121j',
+    D: 'Wup331BgFuytt788o',
+    E: 'Vja882Gptlklgjjk',
+    F: 'CbO016Dxmipoifdg5456',
+    G: 'Jpr104Gol654rtetfgh',
+    H: 'Bss541Lky987ljhlgh2313s',
+    I: 'FtH029Lgtjkhnbspoipfg',
+    J: 'Mer710Gnr132654987',
+    K: 'Qop487Rdtkmlkfgh5646aze',
+    L: 'Bod452VViiuher8787fd',
+    M: 'Wqs418Rtbsdfg7987ds',
+    N: 'SpS891Ntpztzret87ty',
+    O: 'Xod038Fbzfghfdh87df',
+    P: 'ApG097Bgtzrte787dfg',
+    Q: 'Pvi726Butcvbqs45re',
+    R: 'Foo00451xcv45fdtyr',
+    S: 'Kpy369Mbvzzret7987',
+    T: 'PdR780Bop78ret89z7g',
+    U: 'Mvp442Srifdsg7987sdg',
+    V: 'Nrs598Ftp87rfdxgxd8',
+    W: 'Cof591Fopoiup87uipou',
+    X: 'Jsa419Ygpazer878fdsd',
+    Y: 'Rij126Prtezrt5454sdfg',
+    Z: 'QQo447FFtxvxw7987',
 
 }]
 
-
-
-
-const link = `https://www.ianimes.org/listing.php?affichage=${A}&b1u3vv0lSorJk9Lex0tbKZEtbz8RlMC9`
+// const link = `https://www.ianimes.org/listing.php?affichage=${A}&b1u3vv0lSorJk9Lex0tbKZEtbz8RlMC9`
 
               ///////////////////////  liste des animes par genres  ////////////////////////////
 
@@ -56,5 +51,5 @@ module.exports = {
     Domaine,
     TopAnime,
     AnimeInfo,
-    page
+    page,
 }
